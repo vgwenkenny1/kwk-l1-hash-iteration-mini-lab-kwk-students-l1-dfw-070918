5037769aca912b34115f594e443f18077d509df1
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-11 19:24:00 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,11 +6,7 @@ end
 puts create_olympics_hash
 
 def add_a_key_value_pair
-summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
 
-new_location = :Atlanta
-new_year = "1996"
-summer_olympics[new_location] = new_year
 
 end
 
