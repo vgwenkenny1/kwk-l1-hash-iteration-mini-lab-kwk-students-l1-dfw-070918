2fba6a65b18589a90c8f96a6b1f98af8d8bd6649
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-11 19:32:11 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -18,7 +18,8 @@ def iterate_through_hash
   summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
   summer_olympics.each do |place, year|
     puts "The #{year} summer olympics took palce in #{place}."
-end
+  end
+end 
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
