c3d89a6091eb533f9888ab5a647f11884d1ebbea
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-11 19:29:47 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -15,7 +15,7 @@ puts add_a_key_value_pair
 
 
 def iterate_through_hash
-  
+  summer_olympics = {:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012"}
 end
 
 def iterate_through_keys
